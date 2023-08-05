b64ff11dafb4aad6f39d63b1fc3692b635629d1f
unknown <2250947274@qq.com>
2023-08-05 13:57:14 +0800

    教程修改版

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-pip install --upgrade pip; pip install poetry;  # 重新安装一下最新的 Poetry，因为默认的 Poetry 的版本比较低。
-poetry install
-
-python manage.py collectstatic --no-input
-python manage.py migrate
